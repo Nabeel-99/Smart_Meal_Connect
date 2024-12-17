bec959a218cce2da4da2ceaabf52dcadd7b4edbf
Farouk Nabeel <farouknabil810@gmail.com>
2024-12-17 01:31:13 +0100

    fixed issue

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -128,8 +128,8 @@ export const logout = (req, res) => {
   try {
     res.clearCookie("token", {
       httpOnly: true,
-      // secure: process.env.NODE_ENV === "production",
-      // sameSite: "None",
+      secure: "false",
+      sameSite: "None",
       // domain: "51.20.120.120",
     });
     return res.status(200).json({ message: "log out successful" });
