45eb4f609af0e322cb6f94833e376fda964f3258
Nabeel Umar <117419091+Nabeel-99@users.noreply.github.com>
2024-12-18 02:21:26 +0100

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -128,9 +128,8 @@ export const logout = (req, res) => {
   try {
     res.clearCookie("token", {
       httpOnly: true,
-      // secure: "false",
-      // sameSite: "None",
-      // // domain: "51.20.120.120",
+  secure: process.env.NODE_ENV === 'production', 
+  sameSite: "None",
     });
     return res.status(200).json({ message: "log out successful" });
   } catch (error) {
