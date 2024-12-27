b8728f0095a9d56ac12a2827405ef149c7417e95
Farouk Nabeel <farouknabil810@gmail.com>
2024-12-27 16:34:01 +0100

    fixed ui bug

--- client/src/components/ui/GetStartedSection.jsx
+++ client/src/components/ui/GetStartedSection.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 
 const GetStartedSection = () => {
   return (
-    <div className="flex  flex-col px-8 gap-20 md:flex-row items-start md:items-center border-b border-b-[#343333] pt-20 mt-20 pb-20  md:justify-between md:px-16  xl:justify-around bg-gradient-to-b from-[#08090a] to-[#221300] w-full">
+    <div className="flex flex-col px-8 gap-20 md:flex-row items-start md:items-center border-b border-b-[#d1d1d1] dark:border-b-[#343333] pt-20 pb-20 w-full md:justify-between md:px-16  xl:justify-around bg-gradient-to-b from-[#dadada] to-[#e8e8e8] dark:from-[#08090a] dark:to-[#161616]">
       <div className="flex flex-col px-8 gap-20 md:flex-row items-start md:items-center md:justify-between  xl:justify-around w-full 2xl:mx-auto 2xl:container">
         <div>
-          <h2 className="text-2xl text-white  md:text-3xl xl:text-6xl tracking-tighter">
+          <h2 className="text-2xl   md:text-3xl xl:text-6xl tracking-tighter">
             Want meal recommendations?{" "}
             <span className="block">
               Discover the best choices just for You!
@@ -16,7 +16,7 @@ const GetStartedSection = () => {
         <div>
           <Link
             to={"/login"}
-            className="bg-[#e6e6e6] text-black rounded-md px-4 py-2"
+            className="dark:bg-[#e6e6e6] bg-black text-white hover:bg-[#252526] dark:text-black rounded-md px-4 py-2"
           >
             Get Started
           </Link>
